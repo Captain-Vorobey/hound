54f9035c858412874d66783048cbbe1ff6c27b73
Captain-Vorobey <vorobyov_egor@bk.ru>
2020-10-16 10:26:02 +0300

    Added a few changes

--- script.rb
+++ script.rb
@@ -1,11 +1,11 @@
+# frozen_string_literal: true
+
 class Histogram_Generator
   def initialize; end
 
   def self.processString(string:)
     result = if !string.empty?
-
                string.gsub(/[ ,.\r\n]/, ' ').split(' ').each_with_object(Hash.new(0)) { |word, result| result[word] += 1 }
-
              else
                {}
              end
@@ -23,7 +23,7 @@ module App
 		strange and mysterious things would soon be happening all over the
 		country. Mr. Dursley hummed as he picked out his most boring tie for
 		work, and Mrs. Dursley gossiped away happily as she wrestled a screaming
-		Dudley into his high chair.'.freeze
+		Dudley into his high chair.'
   def perform
     text = ''
     puts('reading file')
